b7522c5274c822f1bbd22629e122374526f16c40
Gustavo Camargo <104240187+GustavoCamargodaSilva@users.noreply.github.com>
2024-02-21 14:18:48 -0300

    categoria dto

---
+++ vendas/src/main/java/com/estoque/vendas/dto/CategoriaDTO.java
@@ -0,0 +1,22 @@
+package com.estoque.vendas.dto;
+
+import com.estoque.vendas.entities.Categoria;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class CategoriaDTO {
+
+    private Long codCategoria;
+    private String nome;
+
+    private CategoriaDTO(Categoria entity) {
+        codCategoria = entity.getCodCategoria();
+        nome = entity.getNome();
+    }
+}
