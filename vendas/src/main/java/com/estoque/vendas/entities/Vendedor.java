00cae433478abbe3523f1d725b1b214e2504f955
Gustavo Camargo <104240187+GustavoCamargodaSilva@users.noreply.github.com>
2024-02-21 01:15:09 -0300

    criaçao classe vendedor

---
+++ vendas/src/main/java/com/estoque/vendas/entities/Vendedor.java
@@ -0,0 +1,30 @@
+package com.estoque.vendas.entities;
+
+import com.estoque.vendas.enums.UserRole;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "tb_vendedor")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Vendedor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long codVendedor;
+    private String nome;
+    private LocalDate dataNascimento;
+    private String email;
+    private String senha;
+    private String telefone;
+    private UserRole role;
+    private Filial filial;
+}
